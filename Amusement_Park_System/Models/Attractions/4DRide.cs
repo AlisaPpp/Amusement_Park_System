a4a14dba659683fcdc23e735b7e6258a48c800fe
ValeriiaGal <lera.galych@gmail.com>
2025-11-12 15:02:52 +0100

    4DRide class implemented

--- Amusement_Park_System/Models/Attractions/4DRide.cs
+++ Amusement_Park_System/Models/Attractions/4DRide.cs
@@ -1,5 +1,38 @@
 ﻿namespace Amusement_Park_System;
 
-public class FourDRide {
-    
+[Serializable]
+public class FourDRide : Attraction
+{
+    private double _showDuration;
+    private List<string> _effectTypes;
+
+    public double ShowDuration 
+    { 
+        get => _showDuration;
+        private set
+        {
+            if (value < 0)
+                throw new ArgumentException("Show duration cannot be negative.");
+            _showDuration = value;
+        }
+    }
+
+    public List<string> EffectTypes 
+    { 
+        get => new List<string>(_effectTypes);
+        private set
+        {
+            if (value == null || value.Count == 0)
+                throw new ArgumentException("Effect types must contain at least one effect.");
+            _effectTypes = new List<string>(value);
+        }
+    }
+
+    public FourDRide(string name, int height, int maxSeats, bool vipPassWorks,
+        double showDuration, List<string> effectTypes)
+        : base(name, height, maxSeats, vipPassWorks)
+    {
+        ShowDuration = showDuration;
+        EffectTypes = effectTypes;
+    }
 }
