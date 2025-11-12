fb21b2befc33dc6e81d3a1d44a6ebf76a4c922f3
ValeriiaGal <lera.galych@gmail.com>
2025-11-12 13:35:50 +0100

    attraction changed to public

--- Amusement_Park_System/Models/Attraction.cs
+++ Amusement_Park_System/Models/Attraction.cs
@@ -1,14 +1,16 @@
 ﻿namespace Amusement_Park_System;
-
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 [Serializable]
-class Attraction
+public class Attraction
 {
     public String Name { get; private set;}
     public bool DoesVipWorks { get; private set; }
     public int AttractionHeight { get; private set; }
     public Zone Zone { get; private set; }
-    public String Status { get; set; } = "Active";
+    public AttractionState State { get; private set; } = AttractionState.Active;
     public int MaxSeats { get; private set; }
 
 
