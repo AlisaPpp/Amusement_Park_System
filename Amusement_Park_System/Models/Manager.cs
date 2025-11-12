0c24149faafff19bf6ceaf4bc42843ab458560f5
Hanna Kirieieva <kireevaann2006@gmail.com>
2025-11-12 14:45:08 +0100

    Added Employee, MaitenanceStaff, Manager and Shift classes

--- Amusement_Park_System/Models/Manager.cs
+++ Amusement_Park_System/Models/Manager.cs
@@ -1,6 +1,18 @@
 namespace Amusement_Park_System;
 
-public class Manager
+public class Manager : Employee
 {
-    
+    public static int MinYearsOfExperience = 3;
+
+    public Manager(
+        string name,
+        string surname,
+        string contactInfo,
+        DateTime birthDate,
+        int yearsOfExperience)
+        : base(name, surname, contactInfo, birthDate, yearsOfExperience)
+    {
+        if (YearsOfExperience < MinYearsOfExperience)
+            throw new ArgumentException($"Managers must have at least {MinYearsOfExperience} years of experience.");
+    }
 }
--- Amusement_Park_System/Models/Shift.cs
+++ Amusement_Park_System/Models/Shift.cs
@@ -2,5 +2,18 @@ namespace Amusement_Park_System;
 
 public class Shift
 {
-    
+    public Shift(DateTime date, DateTime startTime, DateTime endTime)
+    {
+        Date = date.Date;
+        StartTime = startTime;
+        EndTime = endTime;
+
+        if (EndTime <= StartTime)
+            throw new ArgumentException("Shift EndTime must be after StartTime.");
+    }
+
+    public DateTime Date { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+
 }
