f97147bc6b606f7e686a7d175dc6610957995454
ValeriiaGal <lera.galych@gmail.com>
2025-11-12 14:24:42 +0100

    lightAttraction implemented and added optional and multi value attribute in extremeAttraction

--- Amusement_Park_System/Models/Attractions/ExtremeAttraction.cs
+++ Amusement_Park_System/Models/Attractions/ExtremeAttraction.cs
@@ -4,23 +4,24 @@ namespace Amusement_Park_System;
 public class ExtremeAttraction : Attraction
 {
     public int MinimumAge { get; private set; }
-    public int MinimumHeight { get; private set; }
-    public string SafetyRestrictions { get; private set; }
+    public int MinimumHeightRequirement { get; private set; }
+    public List<string>? SafetyRestrictions { get; private set; }
+
 
     public ExtremeAttraction(string name, int height, int maxSeats, bool vipPassWorks,
-                           int minimumAge, int minimumHeight, string safetyRestrictions)
+                           int minimumAge, int minimumHeightRequirement, string safetyRestrictions)
         : base(name, height, maxSeats, vipPassWorks)
     {
         if (minimumAge < 12)
             throw new ArgumentException("Extreme attractions require minimum age of 12 years");
-        if (minimumHeight < 140)
+        if (minimumHeightRequirement < 140)
             throw new ArgumentException("Extreme attractions must have minimum height of 140cm");
         if (string.IsNullOrWhiteSpace(safetyRestrictions))
             throw new ArgumentException("Safety restrictions cannot be empty");
 
         this.MinimumAge = minimumAge;
-        this.MinimumHeight = minimumHeight;
-        this.SafetyRestrictions = safetyRestrictions;
+        this.MinimumHeightRequirement = minimumHeightRequirement;
+        this.SafetyRestrictions = new List<string>();
     }
 
 
--- Amusement_Park_System/Models/Attractions/LightAttraction.cs
+++ Amusement_Park_System/Models/Attractions/LightAttraction.cs
@@ -1,6 +1,22 @@
 namespace Amusement_Park_System;
 
-public class LightAttraction
+[Serializable]
+public class LightAttraction : Attraction
 {
+   
+    public int MinimumHeightRequirement { get; private set; }
+    public bool IsParentSupervisionRequired { get; private set; }
     
+
+    public LightAttraction(string name, int height, int maxSeats, bool vipPassWorks,
+        int minimumHeightRequirement, bool isParentSupervisionRequired )
+        : base(name, height, maxSeats, vipPassWorks)
+    {
+        if (minimumHeightRequirement < 100)
+            throw new ArgumentException("Light attractions must have minimum height of 100cm");
+        
+        this.MinimumHeightRequirement = minimumHeightRequirement;
+        this.IsParentSupervisionRequired = isParentSupervisionRequired;
+        
+    }
 }
