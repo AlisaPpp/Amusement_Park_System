4d2f01558861e2bd37f53dd758e926beb571c3e4
ValeriiaGal <lera.galych@gmail.com>
2025-11-12 14:55:50 +0100

    structure of get and set changed on attractions

--- Amusement_Park_System/Models/Attractions/ExtremeAttraction.cs
+++ Amusement_Park_System/Models/Attractions/ExtremeAttraction.cs
@@ -5,7 +5,14 @@ public class ExtremeAttraction : Attraction
 {
     public static int MinimumAge = 12;
     public static int MinimumHeightRequirement = 140;
-    public List<string>? SafetyRestrictions { get; private set; }
+    
+    private List<string>? _safetyRestrictions;
+
+    public List<string>? SafetyRestrictions 
+    { 
+        get => _safetyRestrictions;
+        private set => _safetyRestrictions = value;
+    }
 
 
     public ExtremeAttraction(string name, int height, int maxSeats, bool vipPassWorks, string safetyRestrictions)
--- Amusement_Park_System/Models/Attractions/MediumAttraction.cs
+++ Amusement_Park_System/Models/Attractions/MediumAttraction.cs
@@ -2,11 +2,17 @@ namespace Amusement_Park_System;
 
 [Serializable]
 public class MediumAttraction : Attraction
-
 {
     public const int MinimumHeightRequirement = 120;
     public const int MinimumAge = 8;
-    public bool FamilyFriendly { get; private set; }
+    
+    private bool _familyFriendly;
+
+    public bool FamilyFriendly 
+    { 
+        get => _familyFriendly;
+        private set => _familyFriendly = value;
+    }
     
     public MediumAttraction(string name, int height, int maxSeats, bool vipPassWorks, bool familyFriendly)
         : base(name, height, maxSeats, vipPassWorks)
