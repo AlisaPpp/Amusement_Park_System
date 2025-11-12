7b7555da8c567a3c78d2efe024fbdb05cd9b5ff0
Hanna Kirieieva <kireevaann2006@gmail.com>
2025-11-12 15:11:03 +0100

    Add RideOperator class

--- Amusement_Park_System/Models/RideOperator.cs
+++ Amusement_Park_System/Models/RideOperator.cs
@@ -1,6 +1,32 @@
-namespace Amusement_Park_System;
+namespace Amusement_Park_System.Models;
 
-public class RideOperator
+public class RideOperator : Employee
 {
-    
+    public RideOperator(
+        string name,
+        string surname,
+        string contactInfo,
+        DateTime birthDate,
+        int yearsOfExperience,
+        string operatorLicenceId,
+        bool isFirstAidCertified)
+        : base(name, surname, contactInfo, birthDate, yearsOfExperience)
+    {
+        OperatorLicenceId = operatorLicenceId;
+        IsFirstAidCertified = isFirstAidCertified;
+    }
+
+    private string _operatorLicenceId = "";
+    public string OperatorLicenceId
+    {
+        get => _operatorLicenceId;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException("OperatorLicenceId cannot be empty.");
+            _operatorLicenceId = value.Trim();
+        }
+    }
+
+    public bool IsFirstAidCertified { get; private set; }
+
 }
