d46f22e5e66a16ad9985b691a591f533c8e228ac
ValeriiaGal <lera.galych@gmail.com>
2025-12-11 14:55:03 +0100

    added tests for ticket order association and ticket ticket_type association

---
+++ Amusement_Park_System_UnitTests/AssociationTests/TicketOrderAssociationTests.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using NUnit.Framework;
+using Amusement_Park_System.Models;
+
+namespace Amusement_Park_System_Tests
+{
+    public class TicketOrderAssociationTests
+    {
+        private Customer customer;
+        private Order order;
+        private Order order2;
+        private TicketType type;
+
+        [SetUp]
+        public void Setup()
+        {
+            Customer.ClearExtent();
+            Order.ClearExtent();
+            Ticket.ClearExtent();
+            TicketType.ClearExtent();
+
+            customer = new Customer("John", "Doe", "john@mail.com");
+            order = new Order(1, "Card", customer);
+            order2 = new Order(2, "Cash", customer);
+            type = new TicketType("Regular", false, 100);
+        }
+
+        [Test]
+        public void TestTicketConstructorCreatesBidirectionalAssociation()
+        {
+            var ticket = new Ticket(
+                DateTime.Now.AddDays(1),
+                DateTime.Now.AddDays(2),
+                10,
+                2,
+                type,
+                order);
+
+            Assert.That(order.Tickets, Contains.Item(ticket));
+            Assert.That(ticket.Order, Is.EqualTo(order));
+        }
+
+        [Test]
+        public void TestTicketConstructorWithOrder()
+        {
+            var ticket = new Ticket(
+                DateTime.Now.AddDays(1),
+                DateTime.Now.AddDays(2),
+                null,
+                1,
+                type,
+                order2);
+
+            Assert.That(order2.Tickets, Contains.Item(ticket));
+            Assert.That(ticket.Order, Is.EqualTo(order2));
+            Assert.That(order.Tickets, Does.Not.Contain(ticket));
+        }
+
+        [Test]
+        public void TestTicketConstructorNullOrderThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Ticket(
+                DateTime.Now.AddDays(1),
+                DateTime.Now.AddDays(2),
+                null,
+                1,
+                type,
+                null));
+        }
+
+        [Test]
+        public void TestMultipleTicketsInOrder()
+        {
+            var ticket1 = new Ticket(DateTime.Now.AddDays(1), DateTime.Now.AddDays(2), null, 1, type, order);
+            var ticket2 = new Ticket(DateTime.Now.AddDays(2), DateTime.Now.AddDays(3), 10, 2, type, order);
+            var ticket3 = new Ticket(DateTime.Now.AddDays(3), DateTime.Now.AddDays(4), null, 3, type, order);
+
+            Assert.That(order.Tickets.Count, Is.EqualTo(3));
+            Assert.That(order.Tickets, Contains.Item(ticket1));
+            Assert.That(order.Tickets, Contains.Item(ticket2));
+            Assert.That(order.Tickets, Contains.Item(ticket3));
+            Assert.That(ticket1.Order, Is.EqualTo(order));
+            Assert.That(ticket2.Order, Is.EqualTo(order));
+            Assert.That(ticket3.Order, Is.EqualTo(order));
+        }
+
+        [Test]
+        public void TestTicketDeleteRemovesFromOrder()
+        {
+            var ticket = new Ticket(
+                DateTime.Now.AddDays(1),
+                DateTime.Now.AddDays(2),
+                null,
+                1,
+                type,
+                order);
+
+            ticket.Delete();
+
+            Assert.That(order.Tickets, Does.Not.Contain(ticket));
+            Assert.That(Ticket.Extent, Does.Not.Contain(ticket));
+        }
+        
+
+        [Test]
+        public void TestTotalPriceZeroWhenNoTickets()
+        {
+            Assert.That(order.TotalPrice, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestTicketOrderPropertyIsCorrect()
+        {
+            var ticket = new Ticket(
+                DateTime.Now.AddDays(1),
+                DateTime.Now.AddDays(2),
+                null,
+                1,
+                type,
+                order);
+
+            Assert.That(ticket.Order, Is.EqualTo(order));
+        }
+
+        [Test]
+        public void TestTicketsCollectionIsReadOnly()
+        {
+            var ticket = new Ticket(
+                DateTime.Now.AddDays(1),
+                DateTime.Now.AddDays(2),
+                null,
+                1,
+                type,
+                order);
+
+            var tickets = order.Tickets;
+            Assert.That(tickets, Is.InstanceOf<IReadOnlyCollection<Ticket>>());
+        }
+    }
+}
---
+++ Amusement_Park_System_UnitTests/AssociationTests/TicketTicketTypeAssociationTests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using NUnit.Framework;
+using Amusement_Park_System.Models;
+
+namespace Amusement_Park_System_Tests
+{
+    public class TicketTicketTypeAssociationTests
+    {
+        private TicketType type1;
+        private TicketType type2;
+        private Order order;
+
+        [SetUp]
+        public void Setup()
+        {
+            Customer.ClearExtent();
+            Order.ClearExtent();
+            Ticket.ClearExtent();
+            TicketType.ClearExtent();
+
+            var customer = new Customer("John", "Doe", "john@mail.com");
+            order = new Order(1, "Card", customer);
+            type1 = new TicketType("Regular", false, 100m);
+            type2 = new TicketType("VIP", true, 200m);
+        }
+
+        [Test]
+        public void TestTicketConstructorCreatesBidirectionalAssociation()
+        {
+            var ticket = new Ticket(
+                DateTime.Now.AddDays(1),
+                DateTime.Now.AddDays(2),
+                10,
+                2,
+                type1,
+                order);
+
+            Assert.That(type1.Tickets, Contains.Item(ticket));
+            Assert.That(ticket.TicketType, Is.EqualTo(type1));
+        }
+
+        [Test]
+        public void TestTicketConstructorWithDifferentType()
+        {
+            var ticket = new Ticket(
+                DateTime.Now.AddDays(1),
+                DateTime.Now.AddDays(2),
+                null,
+                1,
+                type2,
+                order);
+
+            Assert.That(type2.Tickets, Contains.Item(ticket));
+            Assert.That(ticket.TicketType, Is.EqualTo(type2));
+            Assert.That(type1.Tickets, Does.Not.Contain(ticket));
+        }
+
+        [Test]
+        public void TestTicketConstructorNullTypeThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Ticket(
+                DateTime.Now.AddDays(1),
+                DateTime.Now.AddDays(2),
+                null,
+                1,
+                null,
+                order));
+        }
+
+        [Test]
+        public void TestMultipleTicketsWithSameType()
+        {
+            var ticket1 = new Ticket(DateTime.Now.AddDays(1), DateTime.Now.AddDays(2), null, 1, type1, order);
+            var ticket2 = new Ticket(DateTime.Now.AddDays(2), DateTime.Now.AddDays(3), 10, 2, type1, order);
+            var ticket3 = new Ticket(DateTime.Now.AddDays(3), DateTime.Now.AddDays(4), null, 3, type1, order);
+
+            Assert.That(type1.Tickets.Count, Is.EqualTo(3));
+            Assert.That(type1.Tickets, Contains.Item(ticket1));
+            Assert.That(type1.Tickets, Contains.Item(ticket2));
+            Assert.That(type1.Tickets, Contains.Item(ticket3));
+            Assert.That(ticket1.TicketType, Is.EqualTo(type1));
+            Assert.That(ticket2.TicketType, Is.EqualTo(type1));
+            Assert.That(ticket3.TicketType, Is.EqualTo(type1));
+        }
+
+        [Test]
+        public void TestTicketDeleteRemovesFromType()
+        {
+            var ticket = new Ticket(
+                DateTime.Now.AddDays(1),
+                DateTime.Now.AddDays(2),
+                null,
+                1,
+                type1,
+                order);
+
+            ticket.Delete();
+
+            Assert.That(type1.Tickets, Does.Not.Contain(ticket));
+            Assert.That(Ticket.Extent, Does.Not.Contain(ticket));
+        }
+        
+
+        [Test]
+        public void TestTicketTypeProperty()
+        {
+            var ticket = new Ticket(
+                DateTime.Now.AddDays(1),
+                DateTime.Now.AddDays(2),
+                null,
+                1,
+                type1,
+                order);
+
+            Assert.That(ticket.TicketType, Is.EqualTo(type1));
+        }
+
+        [Test]
+        public void TestTicketsCollectionIsReadOnly()
+        {
+            var ticket = new Ticket(
+                DateTime.Now.AddDays(1),
+                DateTime.Now.AddDays(2),
+                null,
+                1,
+                type1,
+                order);
+
+            var tickets = type1.Tickets;
+            Assert.That(tickets, Is.InstanceOf<IReadOnlyCollection<Ticket>>());
+        }
+
+        [Test]
+        public void TestMultipleTicketsWithDifferentTypes()
+        {
+            var ticket1 = new Ticket(DateTime.Now.AddDays(1), DateTime.Now.AddDays(2), null, 1, type1, order);
+            var ticket2 = new Ticket(DateTime.Now.AddDays(2), DateTime.Now.AddDays(3), 10, 2, type2, order);
+
+            Assert.That(type1.Tickets.Count, Is.EqualTo(1));
+            Assert.That(type2.Tickets.Count, Is.EqualTo(1));
+            Assert.That(type1.Tickets, Contains.Item(ticket1));
+            Assert.That(type2.Tickets, Contains.Item(ticket2));
+            Assert.That(ticket1.TicketType, Is.EqualTo(type1));
+            Assert.That(ticket2.TicketType, Is.EqualTo(type2));
+        }
+        
+        
+    }
+}
